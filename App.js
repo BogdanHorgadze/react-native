f197d22eaa12af0f47a6e57585846cfd68a21634
Belikov Igor <47883756+belikovdev@users.noreply.github.com>
2020-09-21 19:16:17 +0300

    add doctorsPage

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DoctorsPage from './src/pages/DoctorsPage';
 import LoginPage from './src/pages/LoginPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginPage/>
+    <View>
+      <DoctorsPage/>
     </View>
   );
 }
